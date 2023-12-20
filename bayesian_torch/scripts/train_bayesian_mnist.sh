8f3e2002fda43044ef0730c33caa29bb959c3e93
philip <philip.yijie.zhang@gmail.com>
2023-12-20 16:27:33 +0800

    should be a template

--- bayesian_torch/scripts/train_bayesian_mnist.sh
+++ bayesian_torch/scripts/train_bayesian_mnist.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+project_name='tighter bounds'
+obj="pacm"
 lr=1.0
 batch_size=256
 epochs=40
 mode='train'
 save_dir='./checkpoint/bayesian'
 
-python examples/main_bayesian_mnist.py --lr=$lr --batch-size=$batch_size --epochs=$epochs --mode=$mode --save_dir=$save_dir
+python examples/main_bayesian_mnist.py --project-name=$project_name \
+                                        --obj=$obj \
+                                        --lr=$lr \
+                                        --batch-size=$batch_size \
+                                        --epochs=$epochs \
+                                        --mode=$mode \
+                                        --save_dir=$save_dir
 
